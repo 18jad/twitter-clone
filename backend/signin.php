8449c215404613cf536ac8d2fa43510b2157695c
Jad Yahya <jadoyahya@gmail.com>
2022-09-18 00:53:11 +0300

    Stored user id in cookies

--- backend/signin.php
+++ backend/signin.php
@@ -9,17 +9,21 @@ if (isset($_POST['email'], $_POST['password'])) {
     $password             = $_POST['password'];
     $username             = NULL;
     $user_hashed_password = NULL;
+    $user_id = NULL;
     $result               = [];
     // get password by email
     $user_hashed_password_sql = "SELECT password FROM users WHERE email='$email'";
     // store auth token when user sign in
     $auth_token_sql = "UPDATE users SET auth_token=UUID() WHERE email='$email'";
+    // store user id
+    $user_id_sql = "SELECT user_id FROM users WHERE email='$email'";
     // if user is logging in with username instead email, switch variable to username and change the query
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $username = $email;
         // get password by usename
         $user_hashed_password_sql = "SELECT password FROM users WHERE username='$username'";
         $auth_token_sql           = "UPDATE users SET auth_token=UUID() WHERE username='$username'";
+        $user_id_sql = "SELECT user_id FROM users WHERE username='$username'";
     }
     // fetch the query and get the hashed password
     $user_hashed_password_query = mysqli_query($db, $user_hashed_password_sql)->fetch_row();
@@ -38,8 +42,12 @@ if (isset($_POST['email'], $_POST['password'])) {
             }
             $get_token_query = mysqli_query($db, $get_token_sql)->fetch_row();
             $auth_token      = $get_token_query[0];
+
+            $user_id_query = mysqli_query($db, $user_id_sql)->fetch_row();
+            $used_id = $user_id_query[0];
             // store the token in cookies
             setcookie("auth_token", $auth_token, time() + (86400 * 30), "/");
+            setcookie("user_id", $user_id, time() + (86400 * 30), "/");
             $result = ["status" => 200, "message" => "Successfully signed in, redirecting you now"];
         } else {
             $result = ["status" => 405, "message" => "Uncorrect password, please try again"];
