8264c9e111e87345d10851f58420d5ba044ccedf
Jad Yahya <jadoyahya@gmail.com>
2022-09-18 00:12:09 +0300

    Added sign up functionality

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,8 +1,8 @@
 
 /* 
     Modal: 
-        -Open and close modal
-        -Switch between modals
+        - Open and close modal
+        - Switch between modals
 */
 const signUpBtn        = document.querySelector('.sign-up-btn'),
       signUpModal      = document.querySelector('.sign-up-modal'),
@@ -45,3 +45,57 @@ signInBtn.addEventListener('click', () => {
 signInSwitch.addEventListener('click', () => {
     showModal(signInModal, signUpModal)
 })
+
+/*
+    Sign Up: 
+        - Check inputs and apply conditions
+        - Fetch api and create new user in database
+        - Show success / error message
+*/
+
+const signUpForm      = document.getElementById("signUpForm"),
+      fullName        = document.getElementById('fullName'),
+      signUpUsername  = document.getElementById('signUpUsername'),
+      signUpEmail     = document.getElementById('signUpEmail'),
+      signUpPassword  = document.getElementById('signUpPassword'),
+      confirmPassword = document.getElementById('signUpConfirmPassword'),
+      signUpResult    = document.getElementById('signUpResult');
+
+const createUser = (fullname, username, email, password, repeatedPassword) => {
+    const settings = {
+        // full_name, username, email, `password`
+        method: 'POST',
+        body  : new URLSearchParams({
+            "fullname": fullname,
+            username,
+            email,
+            password,
+            repeatedPassword
+        })
+    }
+    fetch('/twitter-clone/backend/signup.php', settings).then(res => res.json()).then(data => {
+        let status = data.status;
+        if (status == 200) {
+            signUpResult.classList.remove('failed-sign-up');
+            signUpResult.classList.add('success-sign-up');
+            fullName.value        = ""
+            signUpUsername.value  = ""
+            signUpEmail.value     = ""
+            signUpPassword.value  = ""
+            confirmPassword.value = ""
+            setTimeout(() => {
+                signUpResult.textContent = "";
+            }, 3000)
+        } else {
+            signUpResult.classList.remove('success-sign-up');
+            signUpResult.classList.add('failed-sign-up');
+        }
+        signUpResult.textContent = data.message;
+    })
+}
+
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createUser(fullName.value, signUpUsername.value, signUpEmail.value, signUpPassword.value, confirmPassword.value)
+})
+
