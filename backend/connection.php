c38d96c1782f2944568dbd595cc86fa4e87f10ea
Jad Yahya <jadoyahya@gmail.com>
2022-09-17 16:23:36 +0300

    Added connection class and removed unwanted code

--- backend/connection.php
+++ backend/connection.php
@@ -1,58 +1,22 @@
 <?php
-if(isset($_POST['submit'])){
-  $full_name = $_POST['name'];
-  $user_name = $_POST['username'];
-  $email = $_POST['email'];
-  $new_password = $_POST['new_password'];
-  
-
-  $host = "localhost";
-  $username = "twitterdb_user";
-  $password = "123123";
-  $dbname = "twitterdb";
-
-  $con = mysqli_connect($host, $username, $password, $dbname);
-  if (!$con){
-    die("Connection failed!" . mysqli_connect_error());
+class Connection
+{
+  protected static $host = "localhost";
+  protected static $username = "root";
+  protected static $password = "";
+  protected static $db_name = "twitterclone";
+  public function __construct()
+  {
   }
 
-  
-  $sql = "INSERT INTO users (user_id, full_name, user_name, email, password) VALUES ('', '$full_name','$user_name', '$email', '$new_password')";
-  $rs = mysqli_query($con, $sql);
-  if($rs){
-    echo "Message has been sent successfully!";
+  public static function connect()
+  {
+    try {
+      $connect = new mysqli(self::$host, self::$username, self::$password, self::$db_name);
+    } catch (Exception $e) {
+      $error = mysqli_error($connect);
+      echo "Connection failed: " . $error . " " . $e->getMessage();
+    }
+    return $connect;
   }
-  else{
-    echo "Error: " . $sql . "<br>" . $con->error;
-  }
-  
 }
-
-if(isset($_POST['signin'])){
-  $user_name = $_POST['user_name'];
-  $new_password = $_POST['password'];
-  $email = $_POST['email'];
-
-  $host = "localhost";
-  $username = "twitterdb_user";
-  $password = "123123";
-  $dbname = "twitterdb";
-
-  $con = mysqli_connect($host, $username, $password, $dbname);
-  if (!$con){
-    die("Connection failed!" . mysqli_connect_error());
-  }
-
-  
-  $sql = "INSERT INTO users (user_id, user_name, email, password) VALUES ('', '$user_name', '$email', '$new_password')";
-  $rs = mysqli_query($con, $sql);
-  if($rs){
-    echo "Message has been sent successfully!";
-  }
-  else{
-    echo "Error: " . $sql . "<br>" . $con->error;
-  }
-  
-}
-
-?>
