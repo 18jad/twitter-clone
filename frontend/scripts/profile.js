959567c2d0922023439b566da57383f55b44b7ff
Jad Yahya <jadoyahya@gmail.com>
2022-09-18 04:26:45 +0300

    Loaded user details when page load

--- frontend/scripts/profile.js
+++ frontend/scripts/profile.js
@@ -1,3 +1,8 @@
+/**
+ * Modal:
+ *  - Open and close
+ */
+
 const openEditModal = document.querySelector('.web-edit'),
     editModal = document.querySelector('.edit-profile-modal'),
     closeEditModal = document.querySelector('.close-modal');
@@ -9,3 +14,39 @@ openEditModal.addEventListener('click', () => {
 closeEditModal.addEventListener('click', () => {
     editModal.classList.remove('show-edit-modal');
 })
+
+
+/**
+ * Get user infromation:
+ *      - Get username / fullname / bio
+ */
+
+const newName = document.getElementById('newName'),
+    newUsername = document.getElementById('newUsername'),
+    newBio = document.getElementById('newBio'),
+    editForm = document.querySelector('.edit-profile-form'),
+    fullNameText = document.getElementById('fullName'),
+    usernameText = document.getElementById('username'),
+    bioText = document.querySelector('.bio-text'),
+    bioFullName = document.getElementById('bioFullName'),
+    bioUsername = document.getElementById('bioUsername');
+
+const getUserDetails = (user_id) => {
+    fetch(`/twitter-clone/backend/getUserInfo.php/?user_id=${user_id}`)
+        .then(res => res.json())
+        .then((data) => {
+            fullNameText.textContent = data.fullName;
+            bioFullName.textContent = data.fullName;
+            usernameText.textContent = `@${data.username}`;
+            bioUsername.textContent = `@${data.username}`;
+            bioText.textContent = data.bioText;
+            newName.value = data.fullName;
+            newUsername.value = data.username;
+            newBio.value = data.bioText;
+        })
+}
+
+window.onload = () => {
+    let user_id = getCookie('user_id');
+    getUserDetails(user_id)
+}
