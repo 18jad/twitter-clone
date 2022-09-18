09c3034f208dba4af4bbd4e21a7821cf37a1d19e
Jad Yahya <jadoyahya@gmail.com>
2022-09-18 13:13:28 +0300

    Added id

--- backend/getUserInfoUsername.php
+++ backend/getUserInfoUsername.php
@@ -7,17 +7,18 @@ $db = $connection->connect();
 if (isset($_GET["username"])) {
     $username = $_GET["username"];
     // get user name, username, bio, profile pic and cover pic sql query
-    $sql_query = "SELECT full_name, username, bio_text, user_profile, user_cover FROM users WHERE username = '$username'";
+    $sql_query = "SELECT `user_id`, full_name, username, bio_text, user_profile, user_cover FROM users WHERE username = '$username'";
 
     // excute this query and return the first row
     $query = mysqli_query($db, $sql_query)->fetch_row();
     // store return resutls into variables using column index
     $result = [
-        "fullName" => $query[0],
-        "username" => $query[1],
-        "bioText" => $query[2],
-        "userProfile" => $query[3],
-        "userCover" => $query[4],
+        "userId" => $query[0],
+        "fullName" => $query[1],
+        "username" => $query[2],
+        "bioText" => $query[3],
+        "userProfile" => $query[4],
+        "userCover" => $query[5],
     ];
     // return result as json
     echo json_encode($result);
